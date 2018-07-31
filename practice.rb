5e19060ccca21a1cab6f114a5756df90ecdc264a
Jason Solomon <jasonmaxsolomon@gmail.com>
2018-07-31 15:43:46 +0000

    automatically backed up by learn

---
+++ practice.rb
@@ -0,0 +1,60 @@
+if x > 5
+  print "I am big"
+elsif x == 5 
+  print "I am medium"
+else 
+  print "I am small"
+end 
+
+# x = 25
+#   if x > 50
+#     print "I am young!"
+#   elsif x == 60 
+#     print "I am in the golden age!"
+#   else 
+#     print "I am ready to retire!"
+#   end 
+ 
+
+def time_of_day(time)
+if time >= 7 && time < 11
+  		puts "Good morning!"
+elsif time >= 11 && time <17
+		puts "Good afternoon!"
+elsif time >= 17 && time < 20        
+		puts "Good evening!"
+else
+  		puts "Good night!"
+end
+end
+
+time_of_day(8) 
+
+
+# x = 5
+#   if x >= 6 && x <= 7
+#     print "wake up"
+#   elsif x >= 7 && x<=9
+#     print "feed dog"
+#   elsif x >= 9 && x <= 11
+#     print "think about life"
+#   else 
+#     print "do nothing and waste time"
+# end
+
+def congrats
+  puts "Congratulations Karlie and Joshua!"
+end 
+
+congrats
+
+
+
+
+
+
+
+
+
+
+
